f7a67dc7d9f8285a6d351a1a171d61e244149998
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-13 23:07:05 +0530

    add css styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import '%PUBLIC_URL%/static/css/style.css';
+import  "./components/style.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
