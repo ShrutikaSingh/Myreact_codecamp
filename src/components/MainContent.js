b6e37672cd10c32f6ca2a30295114cbb788b2487
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-13 23:05:01 +0530

    using %PUBLIC_URL%/ for the folder outside the src folder

---
+++ src/components/MainContent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainContent(){
+return (
+    <div>
+      <input type="checkbox" name="todo1"/>
+      <p>Sending Mails</p>
+      <input type="checkbox" name="todo2"/>
+       <p>Reading FA</p>
+      <input type="checkbox" name="todo3"/> 
+      <p>Call Jethitech</p>
+    </div>
+)    
+}
+export default MainContent;
