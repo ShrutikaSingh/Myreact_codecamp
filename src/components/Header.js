dc8c14aac8c74e1045b72de6489af07a69032062
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-13 22:32:11 +0530

    className vs classList

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 //use ClassName insted of class in react
-
+//sice class is reserved key in vanilla js
 function Header(){
     return (
         <p>This is header</p>
@@ -9,3 +9,17 @@ function Header(){
 }
 
 export default Header;
+
+//in vanilljs 
+
+/*
+//Get the <div> element with id="myDIV" and add the "new-class-name" class to it:
+document.getElementById("myDiv").className+="new-class-name"
+
+//Get the <div> element with id="myDIV" and add the "mystyle" class to it:
+function myFunction() {
+  var element = document.getElementById("myDIV");
+  element.classList.add("mystyle");
+}
+//The classList property is not supported in Internet Explorer 9. The following code will work in all browsers:
+*/
