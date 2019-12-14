d39fa8e518d136e79e055898a9440cd0c53a3c53
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-14 19:52:48 +0530

    update showTime()

--- src/components/daygreeting.js
+++ src/components/daygreeting.js
@@ -1,11 +1,27 @@
 //write a program to wish good morning, evening and afternoon a/c to hours of day
 import React from "react";
+{/*
+function greetings(){
+let greeting;
+const hoursofday=date.getHours();
+  if(hoursofday<12){
+      greeting="Good morning";
+  }
+  else if (hoursofday>=12 && hoursofday<=17){
+      greeting="Good Evening";
+  }
+  else{
+      greeting="Good Night"
+  }
+return {greeting};
+}
 
-function showTime(){
-  const date=new Date();
-  console.log(date.getHours());
-  return (<p>it is currently about {date.getHours()%12} o'clock.</p>)
+*/}
+
+function dates(){
+  const date=new Date().getHours();
+  return (<p>it is currently about {date%12} o'clock.</p>)
 }
 
 
-export default showTime;
+export default dates;
