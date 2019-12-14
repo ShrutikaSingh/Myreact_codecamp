0de5c04f2e32b825e7e3f23dccce5a240290c10c
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-14 20:45:19 +0530

    print the current date using Date() function

--- src/components/daygreeting.js
+++ src/components/daygreeting.js
@@ -1,29 +1,16 @@
 //write a program to wish good morning, evening and afternoon a/c to hours of day
 import React from "react";
+//printing current date
 
-
-function greetings(){
-let greeting;
-const date= new Date(2018); // change the date to 2018 and it will take the day time so the color will be red
+function currentDate(){
+const dates= new Date();
+const printdate= dates.getFullYear()+'-'+parseInt(dates.getMonth()+1)+'-'+dates.getDate();
 const styles={
  color:"purple"
 }
-const hoursofday=date.getHours();
-  if(hoursofday<12){
-      greeting="morning"
-      styles.color="red"; //since styles is a object we can access its property using . operator
-  }
-  else if (hoursofday>=12 && hoursofday<=17){
-      greeting="Evening";
-      styles.color="blue";
-  }
-  else{
-      greeting="Night";
-      styles.color="blue";
-  }
-return (
-  <p style={styles}>Good {greeting}</p>
 
+return (
+  <p style={styles}>Today Date is : {printdate}</p>
 )
 }
-export default greetings;
+export default currentDate;
