4661c90eaecbfb588addea261ff127d9c4b60251
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-14 20:27:04 +0530

    more about styling and variables inside function

--- src/components/daygreeting.js
+++ src/components/daygreeting.js
@@ -1,24 +1,28 @@
 //write a program to wish good morning, evening and afternoon a/c to hours of day
 import React from "react";
-const styles={
-  backgroundColor:"red", color:"purple"
-}
+//we can change the styles a/c to setting the function variable by taking the styles variable inside the function
 
 function greetings(){
 let greeting;
 const date= new Date();
+const styles={ // we can change the styles by taking the styles variable by taking the styles variable inside the function
+  backgroundColor:"red", color:"purple"
+}
 const hoursofday=date.getHours();
   if(hoursofday<12){
-      greeting="morning";
+      greeting="morning"
+      styles.color="blue";
   }
   else if (hoursofday>=12 && hoursofday<=17){
       greeting="Evening";
   }
-  else{
-      greeting="Night"
+  else{ // we can change the styles by taking the styles variable by taking the styles variable inside the function
+      greeting="Night";
+      styles.color="blue"; //don't use , here
+      styles.backgroundColor="black";
   }
 return (
-  <p style={styles}>Good {greeting} </p> //this type of styling will not work we have to add one for {} since its a javascript not jsx
+  <p style={styles}>Good {greeting} </p>
 )
 }
 export default greetings;
