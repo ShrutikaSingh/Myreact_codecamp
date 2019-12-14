43026139a6eec043a6c8363baafe09e4173cc3bf
ShrutikaSingh <shrutika051220@gmail.com>
2019-12-14 19:49:30 +0530

    add showTime function to show the time in 12hours format

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import  "./components/style.css";
 import Header from './components/Header';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
-
+import ShowTime from './components/daygreeting';
 //./ means from current directory
 //a functional component must return something
 //see a functiononal component can return a functionional component since MyInfo is also a functional component
 
-function App(){ 
+function App(){
   return (
     <div>
+      <input type="checkbox" name="todo1"/>
       <Header/>
       <MainContent/>
       <Footer/>
+      <ShowTime/>
+    {/* <p>{Greeting.greeting(15)}</p>
+     <Greeting> <p> this is {greetings.greeting}</p></Greeting> */}
     </div>
     )
 }
